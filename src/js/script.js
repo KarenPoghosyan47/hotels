8dc31f7045332d70ee3622a65775f1f7c0aa4336
Karen <153460525+KarenPoghosyan47@users.noreply.github.com>
2024-10-26 01:48:00 +0400

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+import Lightpick from "lightpick";
+import "moment"
+
+// new Lightpick({ field: document.getElementById('datepicker') });
+
+var picker = new Lightpick({
+
+    field: document.getElementById('datapicker-start'),
+    secondField: document.getElementById('datapicker-end'),
+    singleDate: false,
+    numberOfMonths: 2,
+
+});
+
+
+var picker = new Lightpick({
+
+    field: document.getElementById('datapicker-start1'),
+    secondField: document.getElementById('datapicker-end1'),
+    singleDate: false,
+    numberOfMonths: 2,
+
+});
+
+
